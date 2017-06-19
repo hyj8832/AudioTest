36251b394e9c5cf0b7d0d39f66f80a4767b81bdb
hyj8832 <hyj8832@naver.com>
2017-06-19 19:17:08 +0900

    #3 Switch Event Programming

--- app/src/main/java/com/example/admin/audiotest/MainActivity.java
+++ app/src/main/java/com/example/admin/audiotest/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.admin.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+        Switch switchMusic;
+        MediaPlayer musicPlayer; //MediaPlayer는 음악관련 클래스
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchMusic=(Switch)findViewById(R.id.switch_music); //참조값 대입
+        musicPlayer=MediaPlayer.create(this,R.raw.music);
+        switchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchMusic.isChecked()){ //isChecked는 불리언타입이라 이때true 반환
+                    musicPlayer.start();
+                }
+                else
+                    musicPlayer.pause();
+            }
+        });
+    }
+
+    //액티비티가 중지될때 나타나는
+    @Override
+    protected void onStop() {
+        super.onStop();
+        musicPlayer.stop();
     }
 }
